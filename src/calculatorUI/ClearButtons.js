5228b1a8418f03e0c0ace6b49d021e44e1f6a2a6
Potassiumzz <brainiacgamer444@gmail.com>
2026-03-02 02:29:41 +0545

    prevent the second operand from being deleted when the operator has just been selected
    - Bit of explanation has been done in the comments in this commit, make
      sure to check it out, but again, the only reason for this is to match
      the UI/UX behvaiour of thing that I am trying to replicate.
    
      This behaivour can vary depending on calculators (I think), since
      this is not really a bug or anything.

--- src/calculatorUI/ClearButtons.js
+++ src/calculatorUI/ClearButtons.js
@@ -1,11 +1,21 @@
-import { assignOperands, initialiseCalculator, displayedAnswer } from "../calculatorLogic/CalculatorState.js";
+import {
+  assignOperands,
+  initialiseCalculator,
+  displayedAnswer,
+  switchToSecondNumber,
+} from "../calculatorLogic/CalculatorState.js";
 import { bigTextField, smallTextField } from "./dom.js";
 import { newInput } from "./NumbersAndOperators.js";
 
 export function clearValues(clearType) {
   if (clearType.includes("delete")) {
     if (displayedAnswer) return (smallTextField.textContent = "");
-    if (newInput) return;
+    // prevent the second operand from being deleted when the user has just selected an operator.
+    // for example, first operand = 5, operator = "+", second operand = "", but in the UI, it appears
+    // that the second operand could be the first operand as well (bigTextField === smallTextField).
+    //
+    // There's no major reason for this logic except to match the UI/UX behaviour of... the calculator.
+    if (newInput && switchToSecondNumber) return;
     bigTextField.textContent = bigTextField.textContent.slice(0, -1);
     if (bigTextField.textContent === "") bigTextField.textContent = "0";
   } else if (clearType.includes("clearAll")) {
